7de915f379f79ec87001b6ed6979ebde67d85a4d
SvenJungnickel <svenjungnickel@googlemail.com>
2017-11-25 18:06:24 +1100

    contact form Google ReCaptcha validation

--- web/js/contact_me.js
+++ web/js/contact_me.js
@@ -8,51 +8,8 @@ $(function () {
         submitSuccess: function ($form, event) {
             event.preventDefault(); // prevent default submit behaviour
 
-            var $button = $("#sendMessageButton"),
-                $spinner = $("#spinner");
-            $button.prop("disabled", true); // Disable submit button until AJAX call is complete to prevent duplicate messages
-            $spinner.show(); // Display loading spinner
-
-            $.ajax({
-                url: "/",
-                type: "POST",
-                dataType: "json",
-                // get values from FORM
-                data: $('#contactForm').serialize(),
-                cache: false,
-                success: function (message) {
-                    console.log(message);
-                    // Success message
-                    $('#success').html("<div class='alert alert-success'>");
-                    $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-                        .append("</button>");
-                    $('#success > .alert-success')
-                        .append("<strong>Vielen Dank für Ihre Nachricht. Unser Team wird sich umgehend mit Ihnen in Verbindung setzen.</strong>");
-                    $('#success > .alert-success')
-                        .append('</div>');
-                    //clear all fields
-                    $('#contactForm').trigger("reset");
-                },
-
-                error: function (error) {
-                    console.log(error);
-                    // Fail message
-                    $('#success').html("<div class='alert alert-danger'>");
-                    $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-                        .append("</button>");
-                    $('#success > .alert-danger').append($("<strong>")
-                        .text("Bitte Entschuldigen Sie. Es scheint ein Problem mit dem Mail Server zu geben. Bitte versuchen Sie zu einem späteren Zeitpunkt erneut."));
-                    $('#success > .alert-danger').append('</div>');
-                    //clear all fields
-                    // $('#contactForm').trigger("reset");
-                },
-                complete: function () {
-                    $spinner.hide(0); // Hide loading spinner
-                    setTimeout(function () {
-                        $button.prop("disabled", false); // Re-enable submit button when AJAX call is complete
-                    }, 1000);
-                }
-            });
+            // Google ReCaptcha validation. If successful it will call the 'submitContactForm' function
+            grecaptcha.execute();
         },
         filter: function () {
             return $(this).is(":visible");
@@ -69,3 +26,46 @@ $(function () {
 $('#name').focus(function () {
     $('#success').html('');
 });
+
+function submitContactForm() {
+    var $button = $("#sendMessageButton"),
+        $spinner = $("#spinner");
+    $button.prop("disabled", true); // Disable submit button until AJAX call is complete to prevent duplicate messages
+    $spinner.show(); // Display loading spinner
+
+    $.ajax({
+        url: "/",
+        type: "POST",
+        dataType: "json",
+        // get values from FORM
+        data: $('#contactForm').serialize(),
+        cache: false,
+        success: function (message) {
+            // Success message
+            $('#success').html("<div class='alert alert-success'>");
+            var $success = $('#success > .alert-success');
+            $success.html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;</button>");
+            $success.append("<strong>Vielen Dank für Ihre Nachricht. Unser Team wird sich umgehend mit Ihnen in Verbindung setzen.</strong>");
+            $success.append('</div>');
+
+            //clear all fields
+            $('#contactForm').trigger("reset");
+        },
+
+        error: function (error) {
+            // Fail message
+            $('#success').html("<div class='alert alert-danger'>");
+            var $danger = $('#success > .alert-danger');
+            $danger.html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;</button>");
+            $danger.append("<strong>Bitte Entschuldigen Sie. Es scheint ein Problem mit dem Mail Server zu geben. Bitte versuchen Sie zu einem späteren Zeitpunkt erneut.</strong>");
+            $danger.append('</div>');
+        },
+        complete: function () {
+            $spinner.hide(0); // Hide loading spinner
+            grecaptcha.reset(); // Reset Google ReCaptcha
+            setTimeout(function () {
+                $button.prop("disabled", false); // Re-enable submit button when AJAX call is complete
+            }, 1000);
+        }
+    });
+}
